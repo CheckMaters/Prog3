8c344b34cb922dbc87317e500829efaaa8141f10
CheckMaters <39863034+CheckMaters@users.noreply.github.com>
2018-12-01 20:15:15 -0500

    Add files via upload

---
+++ bankingClient.h
@@ -0,0 +1,7 @@
+#ifndef BANKINGCLIENT_H
+#define BANKINGCLIENT_H
+
+//this will print the options of steps that Client could perform after connecting with Server
+void print_options();
+
+#endif
---
+++ bankingServer.h
@@ -0,0 +1,8 @@
+#ifndef BANKINGSERVER_H
+#define BANKINGSERVER_H
+
+
+void * connection_handler (void * socket_fd);
+
+
+#endif
